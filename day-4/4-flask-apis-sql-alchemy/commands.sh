8563b8553da33d052299281492d8bddbc55b504a
Atin Gupta <atingupta@email.com>
2024-12-13 13:33:53 +0530

    -

--- day-4/4-flask-apis-sql-alchemy/commands.sh
+++ day-4/4-flask-apis-sql-alchemy/commands.sh
@@ -8,13 +8,13 @@ flask run
 curl -X GET http://127.0.0.1:5000/employees
 
 # Create a New Employee
-curl -X POST http://127.0.0.1:5000/employee -H "Content-Type: application/json" -d "{\"name\": \"Atin Gupta\", \"position\": \"Software Engineer\", \"department\": \"Engineering\"}"
+curl -X POST http://127.0.0.1:5000/employee -H "Content-Type: application/json" -d "{\"name\": \"Amit Gupta\", \"position\": \"Software Engineer\", \"department\": \"Engineering\"}"
 
 # Get a Specific Employee
 curl -X GET http://127.0.0.1:5000/employee/1
 
 # Update a Specific Employee
-curl -X PUT http://127.0.0.1:5000/employee/1 -H "Content-Type: application/json" -d "{\"name\": \"Atin Kumar\", \"position\": \"Senior Software Engineer\", \"department\": \"Engineering\"}"
+curl -X PUT http://127.0.0.1:5000/employee/2 -H "Content-Type: application/json" -d "{\"name\": \"Atin Kumar\", \"position\": \"Senior Software Engineer\", \"department\": \"Engineering\"}"
 
 # Delete a Specific Employee
 curl -X DELETE http://127.0.0.1:5000/employee/1
